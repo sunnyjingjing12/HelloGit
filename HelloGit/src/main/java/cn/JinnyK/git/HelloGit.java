8444a3690d7c4e079f597ac6f5f907c0708ccfc5
sunnyjingjing12 <987853593@qq.com>
2022-05-07 06:33:06 +0800

    主干添加

--- HelloGit/src/main/java/cn/JinnyK/git/HelloGit.java
+++ HelloGit/src/main/java/cn/JinnyK/git/HelloGit.java
@@ -8,6 +8,8 @@ public class HelloGit {
         System.out.println("HelloGit!");
         System.out.println("更新1");
         System.out.println("创建分支dev");
+        System.out.println("主干添加");
+
     }
 
 }
