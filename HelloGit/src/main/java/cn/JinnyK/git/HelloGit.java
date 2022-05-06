de087292fee0b975f2c06880601c12be6e7e45a5
sunnyjingjing12 <987853593@qq.com>
2022-05-07 06:15:26 +0800

    更新1

--- HelloGit/src/main/java/cn/JinnyK/git/HelloGit.java
+++ HelloGit/src/main/java/cn/JinnyK/git/HelloGit.java
@@ -6,6 +6,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("HelloGit!");
+        System.out.println("更新1");
     }
 
 }
